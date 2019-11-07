4a77eb5b042dd907ee6aeceb245cf3d84866e92c
JQueimado <32784376+JQueimado@users.noreply.github.com>
2019-11-07 15:57:32 +0000

    Install Bootstrap

--- Mongodb-Test/mern-example/src/App.js
+++ Mongodb-Test/mern-example/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">

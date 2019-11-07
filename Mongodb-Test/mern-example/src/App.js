7050d3553907118eaef374714d64269faf79494e
JQueimado <32784376+JQueimado@users.noreply.github.com>
2019-11-07 16:09:10 +0000

    Setup Router and Create component TodosList

--- Mongodb-Test/mern-example/src/App.js
+++ Mongodb-Test/mern-example/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,9 +10,14 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h2>MERN</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <h2>MERN</h2>
+      </div>
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
+    </Router>
   );
 }
 

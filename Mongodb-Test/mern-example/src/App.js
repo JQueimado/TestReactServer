1ae4129ef085e142b986b26e828f5ff2ddf35238
JQueimado <32784376+JQueimado@users.noreply.github.com>
2019-11-07 16:20:12 +0000

    Pasted new App.js

--- Mongodb-Test/mern-example/src/App.js
+++ Mongodb-Test/mern-example/src/App.js
@@ -16,12 +16,43 @@ import TodosList from "./Components/todoList";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <h2>MERN</h2>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a
+            class="navbar-brand"
+            href="https://codingthesmartway.com"
+            target="_blank"
+          >
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              alt="CodingTheSmartWay.com"
+            />
+          </a>
+          <Link to="/" className="navbar-brand">
+            MERN-Stack Todo App
+          </Link>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">
+                  Todos
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">
+                  Create Todo
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br />
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
       </div>
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="/create" component={CreateTodo} />
     </Router>
   );
 }

56089637b6f7553a13b47795380b119c10900d57
JQueimado <jpqueimado@gmail.com>
2019-11-08 16:33:47 +0000

    Setup Atlas Connection

--- Mongodb-Test/backend/server.js
+++ Mongodb-Test/backend/server.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const mongose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 4000;
 
+require("dotenv").config();
+
 app.use(cors());
 app.use(bodyParser.json());
+
+const uri = process.env.ATLAS_URI;
+mongose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+const connection = mongose.connection;
+
+connection.once("open", () => {
+  console.log("Connected to Atlas");
+});
+
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
 });

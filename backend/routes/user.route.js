cf2f67710b69867053fc91d0bd9774ad32581d8c
JQueimado <jpqueimado@gmail.com>
2019-11-11 12:06:44 +0000

    Routes Improvement

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,18 @@
 const userRouter = require("express").Router();
 let User = require("../models/user.model");
 
+userRouter.route("/").get((request, response) => {
+  User.find((err, users) =>
+    err ? console.log("Users could not be found") : response.json(users)
+  );
+});
+
 userRouter.route("/add").post((request, response) => {
   let new_User = new User(request.body);
   new_User
-    .save(err => {
-      err ? res.status(404).send("user could not be added") : pass;
-    })
-    .then(user => res.status(200).json({ user: "user added sucsesfuly" }));
+    .save()
+    .then(user => response.status(200).json({ user: "user added sucsesfuly" }))
+    .catch(err => response.status(404).send("user could not be added"));
 });
 
 userRouter.route("/:id").get((request, response) => {

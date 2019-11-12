641d1fb4f43c9d26d72d3053b2970ab3483879b0
JQueimado <32784376+JQueimado@users.noreply.github.com>
2019-11-12 19:48:24 +0000

    Add Create User

--- My_App/backend/models/user.model.js
+++ My_App/backend/models/user.model.js
@@ -2,7 +2,11 @@ const mongose = require("mongoose");
 const Schema = mongose.Schema;
 
 let User = new Schema({
-  user_name: { type: String }
+  user_name: {
+    type: String,
+    trim: true,
+    required: true
+  }
 });
 
 module.exports = mongose.model("User", User);
--- My_App/my_forum/src/Components/CreateUser.jsx
+++ My_App/my_forum/src/Components/CreateUser.jsx
@@ -1,11 +1,101 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      user_name: "",
+      password: "",
+      email: ""
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeUsernameDescription = this.onChangeUsernameDescription.bind(
+      this
+    );
+    this.onChangePasswordDescription = this.onChangePasswordDescription.bind(
+      this
+    );
+    this.onChangeEmailDescription = this.onChangeEmailDescription.bind(this);
+  }
+
+  // Form Controlers
+  onChangeUsernameDescription(e) {
+    this.setState({ user_name: e.target.value });
+  }
+
+  onChangePasswordDescription(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  onChangeEmailDescription(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    if (this.user_name === "") return;
+
+    const Package = {
+      user_name: this.state.user_name
+    };
+
+    axios.post("http://localhost:4000/user/add", Package).then(res => {
+      console.log(res.data);
+    });
+  }
+
+  // Style Controlers
+  usernameStyler() {
+    const normalClass = "form-control";
+
+    return normalClass;
   }
 
   render() {
-    return <div> This is Create class </div>;
+    return (
+      <div className="m-4">
+        <h3> Create User </h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label> User Name: </label>
+            <input
+              type="text"
+              className={this.usernameStyler()}
+              value={this.state.user_name}
+              onChange={this.onChangeUsernameDescription}
+            ></input>
+          </div>
+          <div className="form-group">
+            <label> Password: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.onChangePasswordDescription}
+            ></input>
+          </div>
+          <div className="form-group">
+            <label> Email: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeEmailDescription}
+            ></input>
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
